f9f162fae329f962a60494bd55cc867cd7a133e7
Fahad Ali <134847376+FaddyWho@users.noreply.github.com>
2023-09-29 21:23:54 +0500

    first-commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+// const appSettings = {
+//     databaseURL: "https://to-do-a619a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+// }
+
+
+// const inputFieldEl = document.getElementById("input-field")
+// const addButtonEl = document.getElementById("add-button")
+
+// addButtonEl.addEventListener("click", function() {
+//     let inputValue = inputFieldEl.value
+    
+//     console.log(inputValue)
+// })
