9be77b41996dc3b8f4513e9d40d6d877b89f664b
Fahad Ali <134847376+FaddyWho@users.noreply.github.com>
2023-09-30 08:17:07 +0500

    firebase-works

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const appSettings = {
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCDTTGm6M0xy190Bcib6O5ky9PudehrOSg",
-    authDomain: "new-list-15c17.firebaseapp.com",
-    projectId: "new-list-15c17",
-    storageBucket: "new-list-15c17.appspot.com",
-    messagingSenderId: "1069306373343",
-    appId: "1:1069306373343:web:376bd5ad5103a2f7ff756a",
-    databaseURL: "https://new-list-15c17-default-rtdb.asia-southeast1.firebasedatabase.app/"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCDTTGm6M0xy190Bcib6O5ky9PudehrOSg",
+//     authDomain: "new-list-15c17.firebaseapp.com",
+//     projectId: "new-list-15c17",
+//     storageBucket: "new-list-15c17.appspot.com",
+//     messagingSenderId: "1069306373343",
+//     appId: "1:1069306373343:web:376bd5ad5103a2f7ff756a",
+//     databaseURL: "https://new-list-15c17-default-rtdb.asia-southeast1.firebasedatabase.app/"
+//   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const app = initializeApp(appSettings);
   const db = getDatabase(app);
   const moviesInDB = ref(db, "movies")
 
